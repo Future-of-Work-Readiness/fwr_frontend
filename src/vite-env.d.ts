5629115f806aee8725e53a07578e9f73a3d98672
Emmanuel Oghenenyore Olokor <neolokor1@gmail.com>
2025-12-31 21:10:31 +0100

    Initialize Next.js app (replaced Vite React frontend)

--- src/vite-env.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="vite/client" />
-
-interface ImportMetaEnv {
-	readonly VITE_API_URL?: string;
-	readonly VITE_GOOGLE_CLIENT_ID?: string;
-	// readonly VITE_PUBLIC_KEY?: string;
-	// add more env vars here...
-}
-
